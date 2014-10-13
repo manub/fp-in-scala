ff5684ecfa9a13949eeb805fb97e530d4fd9d15e
Emanuele Blanco <emanuele.blanco@gmail.com>
2014-10-13 13:43:49 +0100

    some more exercises for chapter 3

--- src/net/manub/fpinscala/Chapter3.scala
+++ src/net/manub/fpinscala/Chapter3.scala
@@ -54,4 +54,47 @@ object List {
     case Cons(x, xs) if pred(x) => dropWhile(xs, pred)
     case _ => list
   }
+
+  def init[A](list: List[A]): List[A] = list match {
+    case Nil => Nil
+    case Cons(x, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def length[A](as: List[A]) = foldRight(as, 0)((_, i) => i+1)
+
+  @tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
+  def foldLeftUsingRight[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
+    def inverse[A, B](f: (B, A) => B): (A, B) => B = {
+      (a, b) => f(b, a)
+    }
+
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldRight(xs, f(z, x))(inverse(f))
+    }
+  }
+
+  def foldRightUsingLeft[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
+    def inverse[A, B](f: (A, B) => B): (B, A) => B = {
+      (a, b) => f(b, a)
+    }
+
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, inverse(f)(z, x))(inverse(f))
+    }
+  }
 }
