c52a1a3b4d5bc9eab198908ff84158f3f6940508
Emanuele Blanco <emanuele.blanco@gmail.com>
2014-10-19 00:01:49 +0100

    more on Trees

--- src/net/manub/fpinscala/TreeCh3.scala
+++ src/net/manub/fpinscala/TreeCh3.scala
@@ -10,4 +10,20 @@ object Tree {
     case Leaf(_) => 1
     case Branch(left, right) => size(left) + size(right)
   }
+
+  def maximum(tree: Tree[Int]): Int = tree match {
+    case Leaf(value) => value
+    case Branch(left, right) => maximum(left) max maximum(right)
+  }
+
+  def depth[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 0
+    case Branch(left, right) => 1 + (depth(left) max depth(right))
+  }
+
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(item) => Leaf(f(item))
+    case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+  }
+
 }
