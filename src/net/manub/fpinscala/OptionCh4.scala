9e045bf078465c817e86a24fa141a2e8a56ee677
Emanuele Blanco <emanuele.blanco@gmail.com>
2014-10-25 18:39:50 +0100

    more work on Options

--- src/net/manub/fpinscala/OptionCh4.scala
+++ src/net/manub/fpinscala/OptionCh4.scala
@@ -31,4 +31,24 @@ object Chapter4 {
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
 
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap { avg => mean(xs map { x => math.pow(x-avg, 2) }) }
+
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
+
+  val absO: Option[Double] => Option[Double] = lift(math.abs)
+
+  def Try[A](a: => A): Option[A] = try Some(a) catch { case e: Exception => None }
+
+  // is this the right way? is it possible to do this without pattern matching?
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    (a, b) match {
+      case (Some(x), Some(y)) => Some(f(x, y))
+      case _ => None
+    }
+
+  def sequence[A](a: scala.List[Option[A]]): Option[scala.List[A]] =
+    Try(a map { elem => elem getOrElse(throw new Exception("!!!")) })
+
+
 }
