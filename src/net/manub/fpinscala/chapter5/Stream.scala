62e5de9c7d1245ff091adaeb1522c0a934f855f7
Emanuele Blanco <emanuele.blanco@gmail.com>
2014-12-12 18:12:37 +0000

    more work on Streams

--- src/net/manub/fpinscala/chapter5/Stream.scala
+++ src/net/manub/fpinscala/chapter5/Stream.scala
@@ -12,10 +12,33 @@ sealed trait Stream[+A] {
   }
 
   def take(n: Int): Stream[A] = if (n == 0) Empty else this match {
-    case Empty => Empty
+    case Empty => Stream.empty
     case Cons(h, t) => Stream.cons(h(), t().take(n-1))
   }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Empty => Stream.empty
+    case Cons(h, t) => if (p(h())) Stream.cons(h(), t().takeWhile(p)) else Stream.empty
+  }
+
+  def exists(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) => p(h()) || t().exists(p)
+    case _ => false
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def existsFoldRight(p: A => Boolean): Boolean = this.foldRight(false)((a, b) => p(a) || b)
+
+  def forAll(p: A => Boolean): Boolean = this.foldRight(true)((a, b) => p(a) && b)
+
+  def takeWhileFolded(p: A => Boolean): Stream[A] =
+    this.foldRight(Stream.empty[A])((a, b) => if (p(a)) Stream.cons(a, b) else Stream.empty)
 }
+
 case object Empty extends Stream[Nothing]
 case class Cons[+A](head: () => A, tail: () => Stream[A]) extends Stream[A]
 
